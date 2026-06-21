b4dc63092bc673c7ea46a03315f4412d87f7a8db
Sijan666 <666majharulislam@gmail.com>
2026-06-21 23:59:25 +0600

    remove unwanted lines

--- frontend/src/components/layouts/RootLayouts.jsx
+++ frontend/src/components/layouts/RootLayouts.jsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom"
-// import Header from "./Header"
-// import Footer from "./Footer"
-
 
 const RootLayouts = () => {
     return (
         <>
-            {/* <Header/> */}
             <Outlet/>
-            {/* <Footer/> */}
         </>
     )
 }
