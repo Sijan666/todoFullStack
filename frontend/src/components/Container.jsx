a57126cdeb0b73a9d256b83175a811d29d972da2
Sijan666 <666majharulislam@gmail.com>
2026-06-21 23:57:33 +0600

    update container

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -2,7 +2,7 @@
 
 const Container = ({children , className}) => {
     return (
-        <div className={`max-w-[330 mx-auto ${className}`}>{children}</div>
+        <div className={`lg:w-330 px-3 lg:px-0 mx-auto ${className}`}>{children}</div>
     )
 }
 
